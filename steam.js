d4a3e696b80d3c9556feb9874dc1b663a766330b
cmillani <carloseduardomillani@gmail.com>
2016-12-23 19:55:23 -0200

    Back to the backend :(

--- steam.js
+++ steam.js
@@ -1,12 +1,28 @@
-let key = F16C318A2D59910D23C1768549C73324;
+let key = "";
 
 function getListForPlayer(id, done) {
-	$.get(
-		"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + key + "&steamid=" + id + "&format=json",
-		function(data) {
-			done(data);
+	var settings = {
+	  "url": "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/",
+	  "method": "GET",
+	  "headers": {
+	    "cache-control": "no-cache",
+	    "Access-Control-Allow-Origin": "*"
+	  },
+		"crossDomain": true,
+		"data": {
+			"key": key,
+			"steamid": id, 
+			"include_appinfo": 1
+		},
+		"success": function(data) {
+			console.log(data);
+		},
+		"error": function(err) {
+			console.log(err);
 		}
-	);	
+	}
+
+	$.ajax(settings);
 }
 
 function getNameForGame(id) {
